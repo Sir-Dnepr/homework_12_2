236418b6a2f1237861ba5541800f8fbef82d12be
Alex Siritsa <alex.siritsa@bartgroup.co>
2025-07-25 13:05:14 +0300

    Homework 12.2

--- js/click-by-parent.js
+++ js/click-by-parent.js
@@ -3,5 +3,7 @@
 const buttonContainer = document.querySelector('.button-container');
 
 buttonContainer.addEventListener('click', (event) => {
-   alert(`Клікнуто на кнопці: ${event.target.value}`);
+   if (event.target.nodeName === 'BUTTON') {
+      alert(`Клікнуто на кнопці: ${event.target.value}`);
+   }
 });
